fa83d80df12bbd68aaa3a4323a19adc1cc681f09
Kobax13 <124640253+Kobax13@users.noreply.github.com>
2023-03-06 01:43:45 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,23 @@
-var nombreCompleto = "hola mi nombre es axel";
+var nombreCompleto = "Devocion al gato";
 
 
-console.log(nombreCompleto)
+alert("Bienvenido a " + nombreCompleto);
 
 var opcion = prompt("Usted es de San Nicolas de los arroyos?")
-    if( opcion=="si"){
-        alert ("El delivery no tiene cargo")
-    } else{
-        alert("El delivery tiene cargo")
-    }
 
-
+if( opcion=="si"){
+    alert ("El delivery no tiene cargo")
+} else{
+    alert("El delivery tiene cargo")
+}
 //prompt es parecido al alert 
-
-var miNombre = prompt("groso, pone tu nombre");
-var miApellido = prompt("Hola " + miNombre + ", ahora tu apellido capo :)");
-var miEdad = prompt("Ok " + miNombre +" "+ miApellido + ", ahora poneme tu edad master");
-    if (miEdad >= 18){
-        alert("Joya sos mayor :)")
-    } else{
-        alert("sos menor, pedile permiso a tus viejos :/")
-    }
-    
+var miNombre = prompt("groso, pone tu nombre y apellido");
+var miEdad = prompt("Hola " + miNombre + ", ahora tu edad capo :)");
+if (miEdad >= 18){
+    alert("Joya sos mayor " + miNombre + " :)")
+} else{
+    alert("sos menor " + miNombre + ", pedile permiso a tus viejos :/")
+}
 
 console.log(miNombre)
-console.log(miApellido)
-console.log(miEdad) 
+console.log(miEdad)
